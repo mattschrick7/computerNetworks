d51ecf6410a3959f0105d16260a90a02a2ab451f
mattschrick7 <mschric1@jh.edu>
2025-09-08 14:18:29 +0000

    For first assignment

---
+++ client.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define SEND_BUFFER_SIZE 2048
+
+
+/* TODO: client()
+ * Open socket and send message from stdin.
+ * Return 0 on success, non-zero on failure
+*/
+int client(char *server_ip, char *server_port) {
+	int status;
+	struct addrinfo hints;
+	struct addrinfo *servinfo;
+	struct addrinfo *p;
+	char ipstr[INET6_ADDRSTRLEN];
+	
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	status = getaddrinfo(server_ip, server_port, &hints, &servinfo);
+	//I believe servinfo now holds possible IP addresses for the server
+	if (status != 0) {
+		perror("Ip not found");
+	}
+	int socketfd;
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		
+		socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (socketfd == -1) {
+			perror("socket error");
+			continue;
+		}
+		if (connect(socketfd, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("Connection Error");
+			close(socketfd);
+			continue;
+		}
+		break;
+	}
+	if (!socketfd){
+		perror("couldn't connect to socket");
+		return -1;
+	}
+	char buf[SEND_BUFFER_SIZE];
+	while(fgets(buf, sizeof(buf), stdin) != NULL) {
+		ssize_t sent = 0;
+		ssize_t totalSent = 0;
+		ssize_t size = strlen(buf);
+		
+		while (sent < size) {
+
+			sent = send(socketfd, buf + sent, strlen(buf) - sent, 0);
+			if (sent == -1) {
+				if (errno == EINTR) {
+					continue;
+				} else {
+					perror("send");
+					return -1;
+				}
+			}
+			totalSent = totalSent + sent;
+		}
+	}
+	freeaddrinfo(servinfo);
+	close(socketfd);	
+	return 0;
+}
+
+/*
+ * main()
+ * Parse command-line arguments and call client function
+*/
+int main(int argc, char **argv) {
+  char *server_ip;
+  char *server_port;
+
+  if (argc != 3) {
+    fprintf(stderr, "Usage: ./client-c [server IP] [server port] < [message]\n");
+    exit(EXIT_FAILURE);
+  }
+
+  server_ip = argv[1];
+  server_port = argv[2];
+  return client(server_ip, server_port);
+}
---
+++ server.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define QUEUE_LENGTH 10
+#define RECV_BUFFER_SIZE 2048
+
+/* TODO: server()
+ * Open socket and wait for client to connect
+ * Print received message to stdout
+ * Return 0 on success, non-zero on failure
+*/
+int server(char *server_port) {
+    int sockfd, new_fd;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+	
+    int rv;
+    int yes = 1;
+    if ((rv = getaddrinfo(NULL, server_port, &hints, &servinfo)) != 0) {
+	    perror("addrinfo failed");
+    	    return 1;
+    }
+	
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+	    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+		    perror("server:socket");
+		    continue;
+	    }
+
+	    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))==-1) {
+		    perror("setsockopts");
+		    exit(1);
+	    }
+
+	    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+		    close(sockfd);
+		    perror("server:bind");
+		    continue;
+	    }
+	    break;
+
+    }
+	
+    freeaddrinfo(servinfo);
+    if (listen(sockfd, QUEUE_LENGTH) == -1) {
+	    perror("listen");
+	    exit(1);
+    }
+    //main accept loop
+    while(1) {
+	    sin_size = sizeof their_addr;
+	    new_fd = accept(sockfd, (struct sockaddr *)&their_addr,&sin_size);
+	    if (new_fd == -1) {
+		    	perror("accept");
+			continue;
+	    }
+		
+	    char msg[RECV_BUFFER_SIZE];
+	    ssize_t bytes_received = 0;
+	    ssize_t expected_size = RECV_BUFFER_SIZE;
+
+	    while ((bytes_received = recv(new_fd, msg, sizeof(msg) -1, 0)) > 0) {
+		    msg[bytes_received] = '\0'; //null terminate
+		    printf("Received: %s\n", msg);
+
+	    }
+	    if (bytes_received == -1) {
+		    perror("recv");
+	    }
+	    close(new_fd);
+    }
+
+    close(sockfd);
+    return 0;    
+
+
+}
+
+/*
+ * main():
+ * Parse command-line arguments and call server function
+*/
+int main(int argc, char **argv) {
+  char *server_port;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: ./server-c [server port]\n");
+    exit(EXIT_FAILURE);
+  }
+
+  server_port = argv[1];
+  return server(server_port);
+}
