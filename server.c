64d3068347145e10e992696ffbebc87ad9402fd3
mattschrick7 <mschric1@jh.edu>
2025-09-08 17:49:41 +0000

    please work

--- server.c
+++ server.c
@@ -75,7 +75,7 @@ int server(char *server_port) {
 
 	    while ((bytes_received = recv(new_fd, msg, sizeof(msg) -1, 0)) > 0) {
 		    msg[bytes_received] = '\0'; //null terminate
-		    printf("%s\n", msg);
+		    printf(msg);
 
 	    }
 	    if (bytes_received == -1) {
